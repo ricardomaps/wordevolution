e209e34d95e99d6fac0c5f794e5207a25e55ad6c
Ricardo Mapurunga Junior <ricmaps_@LAPTOP-LO9AB2OE>
2025-05-28 20:29:29 -0300

    removed embarassing print lol

--- popredis.py
+++ popredis.py
@@ -15,4 +15,3 @@ with open("etymology.csv") as f:
         key = row["lang"] + ":" + row["term"]
         val = row["related_lang"] + ":" + row["related_term"]
         db.lpush(key, val)
-    print(db.lrange("English:cat", 0, -1))
